28501b246c185d586d2c1a4969930dc986d95c4b
b3m3 <uwm.d.c.a@gmail.com>
2022-12-12 13:26:53 +0200

    opti timer code

--- js/constans.js
+++ js/constans.js
@@ -1,2 +1,18 @@
+// Tabs
 export const tabsBtns = document.querySelectorAll('.header__wrapp li');
 export const tabsContents = document.querySelectorAll('.content');
+
+// Timer
+export const timerStartBlock = document.querySelector('.timer__start');
+export const timerNextBlock = document.querySelector('.timer__clock');
+
+export const timerStartCounter = document.querySelectorAll('.tst');
+export const timerNextCounter = document.querySelectorAll('.tet');
+
+export const timerCancelBtn = document.querySelector('.timer-cancel');
+export const timerStartBtn = document.querySelector('.timer-start');
+export const timerPauseBtn = document.querySelector('.timer-pause');
+
+export const timerHours = document.querySelector('.teh');
+export const timerMinutes = document.querySelector('.tem');
+export const timerSeconds = document.querySelector('.tes');
--- js/index.js
+++ js/index.js
@@ -1,99 +1,72 @@
+import { showCurrentTime, tabs, timerCounter } from './functions.js';
 import { tabsBtns, tabsContents } from './constans.js';
-import { showCurrentTime, tabs } from './functions.js';
+import { 
+  timerStartBlock, 
+  timerNextBlock, 
+  timerStartCounter,
+  timerNextCounter,
+  timerCancelBtn,
+  timerStartBtn,
+  timerPauseBtn,
+  timerHours,
+  timerMinutes,
+  timerSeconds
+} from './constans.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Main Tabs
+  tabsBtns.forEach((btn, i) => {
+    btn.addEventListener('click', () => tabs(tabsBtns, tabsContents, i, 'active'));
+  });
+
+  // Clock
   setInterval(() => {
     showCurrentTime('.arrow-hours', 'getHours', 12);
     showCurrentTime('.arrow-minutes', 'getMinutes', 60);
     showCurrentTime('.arrow-seconds', 'getSeconds', 60);
   }, 1000);
 
-  tabsBtns.forEach((btn, i) => {
-    btn.addEventListener('click', () => tabs(tabsBtns, tabsContents, i, 'active'));
+  // Timer
+  let interval;
+  let pause = false;
+
+  timerPauseBtn.addEventListener('click', () => {
+    pause = !pause
+
+    if (pause) {
+      clearInterval(interval);
+    } else {
+      interval = setInterval(() => {
+        timerCounter(interval, timerHours, timerMinutes, timerSeconds);
+      }, 1000)
+    }
   });
 
-  const timerStartBlock = document.querySelector('.timer__start');
-  const timerNextBlock = document.querySelector('.timer__clock');
-  const timerCancelBtn = document.querySelector('.timer__buttons button:first-child');
-  const timerStartBtn = document.querySelector('.timer__buttons button:last-child');
-
-  const startTimes = document.querySelectorAll('.tst');
-  const endTimes = document.querySelectorAll('.tet');
-
-  const timerHours = document.querySelector('.teh');
-  const timerMinutes = document.querySelector('.tem');
-  const timerSeconds = document.querySelector('.tes');
-
-  let interval;
-  let started = false;
-
-  const handleStatusStartBtn = () => {
-    if (started) {
-      timerStartBtn.textContent = 'Pause';
-    } else {
-      timerStartBtn.textContent = 'Start';
-    }
-  };
-  
   timerStartBtn.addEventListener('click', () => {
-    started = true;
-    handleStatusStartBtn();
-
-    timerStartBtn.textContent = 'Paused';
     timerCancelBtn.classList.add('active');
     timerNextBlock.classList.add('active');
+    timerPauseBtn.classList.add('active');
+    timerStartBtn.classList.add('hidden');
+    timerPauseBtn.classList.remove('hidden');
     timerStartBlock.classList.remove('active');
 
-    startTimes.forEach((t, i) => {
-      endTimes[i].textContent = t.textContent;
+    timerStartCounter.forEach((t, i) => {
+      timerNextCounter[i].textContent = t.textContent;
     });
 
-    // if (+timerMinutes.textContent > 0) {
-    //   if (+timerSeconds.textContent < 0) {
-    //     timerMinutes.textContent = +timerMinutes.textContent - 1;
-    //     timerSeconds.textContent = 2;
-    //   }
-    // }
-
-    // timerHours
-    // timerMinutes
-    // timerSeconds
-
     interval = setInterval(() => {
-      timerSeconds.textContent--;
-
-      if (+timerHours.textContent > 0 && 
-        +timerMinutes.textContent === 0 && 
-        +timerSeconds.textContent < 0)
-      {
-        timerHours.textContent = +timerHours.textContent - 1;
-        timerMinutes.textContent = 3;
-      }
-
-      if (+timerMinutes.textContent > 0 && +timerSeconds.textContent < 0) {
-        timerMinutes.textContent = +timerMinutes.textContent -1;
-        timerSeconds.textContent = 2;
-      }
-
-
-
-      if (+timerHours.textContent === 0 && 
-        +timerMinutes.textContent === 0 && 
-        +timerSeconds.textContent <= 0) 
-      {
-        clearInterval(interval);
-      }
-    }, 1000);
-  })
-
+      timerCounter(interval, timerHours, timerMinutes, timerSeconds);
+    }, 1000)
+  });
+  
   timerCancelBtn.addEventListener('click', () => {
-    started = false;
-    handleStatusStartBtn();
-
     timerStartBlock.classList.add('active');
-    timerCancelBtn.classList.remove('active');
+    timerPauseBtn.classList.add('hidden');
+    timerStartBtn.classList.remove('hidden');
     timerNextBlock.classList.remove('active');
-
+    timerCancelBtn.classList.remove('active');
+    
     clearInterval(interval);
-  })
+    pause = false;
+  });
 });
