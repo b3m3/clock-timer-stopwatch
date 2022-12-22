b26d03041a8daca5370c1bf05474113df1ac6926
b3m3 <uwm.d.c.a@gmail.com>
2022-12-22 23:58:22 +0200

    fixAudio func

--- js/index.js
+++ js/index.js
@@ -13,10 +13,14 @@ import { tabButtons, tabContents, timerFirst, timerNext,  swiperWrappers, timerN
 import { callback, options } from './observer.js';
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
 
-import { ios } from './ios.js';
+import { ios, fixAudio } from './ios.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
+
+  document.addEventListener('touchend', () => window.AudioContext.resume());
+
+  fixAudio();
 
   // MAIN TABS
   tabButtons.forEach((btn, i) => {
--- js/ios.js
+++ js/ios.js
@@ -3,3 +3,39 @@ export const ios = (p, s) => {
     return s.play();
   }
 };
+
+export const fixAudio = () => {
+  
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+	if (window.AudioContext) {
+		window.AudioContext = new window.AudioContext();
+	}
+
+	var fixAudioContext = function (e) {
+		if (window.AudioContext) {
+			// Create empty buffer
+			var buffer = window.AudioContext.createBuffer(1, 1, 22050);
+			var source = window.AudioContext.createBufferSource();
+			source.buffer = buffer;
+			// Connect to output (speakers)
+			source.connect(window.AudioContext.destination);
+			// Play sound
+			if (source.start) {
+				source.start(0);
+			} else if (source.play) {
+				source.play(0);
+			} else if (source.noteOn) {
+				source.noteOn(0);
+			}
+		}
+		// Remove events
+		document.removeEventListener('touchstart', fixAudioContext);
+		document.removeEventListener('touchend', fixAudioContext);
+	};
+  
+	// iOS 6-8
+	document.addEventListener('touchstart', fixAudioContext);
+	// iOS 9
+	document.addEventListener('touchend', fixAudioContext);
+};
