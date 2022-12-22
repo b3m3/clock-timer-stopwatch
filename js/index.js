456f94ac4ac0b66d1681eea842c1c16cd631f8ac
b3m3 <uwm.d.c.a@gmail.com>
2022-12-22 17:48:03 +0200

    swap audio

--- js/index.js
+++ js/index.js
@@ -51,15 +51,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     handleStartTimerCounter(timerInterval, timerHours, timerMinutes, timerSeconds);
 
-    signal.addEventListener('click', (e) => e ? signal.play() : null);
-
     if (Math.ceil(totalPrecent) >= 284) {
       timerPauseBtn.classList.remove('active');
       bell.classList.add('active');
       timerNext.classList.add('scale');
       playSignal(signal);
-      console.log(navigator.userAgent);
     }
+
+    signal.addEventListener('click', (e) => e ? signal.play() : null);
   }; // function for a setIntervals
 
   timerPauseBtn.addEventListener('click', () => {
