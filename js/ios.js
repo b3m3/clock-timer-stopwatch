fa6ceffef4c10d99040ec2e7cedd2ebaa5c1a902
b3m3 <uwm.d.c.a@gmail.com>
2022-12-22 23:10:38 +0200

    cr func

--- js/ios.js
+++ js/ios.js
@@ -1,3 +1,11 @@
 export const ios = () => {
-  console.log(1);
+  const mp3 = new Audio('./audio/signal.mp3');
+  const playBtn = document.createElement('button');
+  playBtn.textContent = 'PLAY';
+
+  document.body.append(playBtn);
+
+  playBtn.onclick = () => {
+    mp3.play();
+  };
 };
