ae54b8d331ba81139a8d9f2298ff072ddda0fa0a
b3m3 <uwm.d.c.a@gmail.com>
2022-12-22 17:39:35 +0200

    remove navigator.userAgent.match(/iPhone|iPad|iPod/i)

--- js/functions.js
+++ js/functions.js
@@ -75,13 +75,7 @@ export const getTotalSeconds = (h, m, s) => {
 };
 
 export const playSignal = audio => {
-  if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
-    audio.addEventListener('click', (e) => e ? audio.play() : null);
-    audio.click();
-    return
-  } else {
-    audio.play();
-  }
+  audio.play();
 };
 
 export const stopSignal = audio => {
